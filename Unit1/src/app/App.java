0f9d1beab1ffbe7a7979c7b697e4ffeec0f86b32
BryanReynoso1 <reynosobryan90@gmail.com>
2020-09-14 15:38:31 +0000

    added top five movies [fixed]

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,6 +2,8 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello mr.Osowski");
-    }
-}
+        System.out.println("1. Star Wars ");
+    System.out.println("2. spider man");
+    System.out.println("3. saving private Ryan");
+System.out.println("4. The Avengers");
+System.out.println("5. Jurassic park");

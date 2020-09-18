170969ecafe8dc8f33df8acdbce1274a84ad0f08
BryanReynoso1 <reynosobryan90@gmail.com>
2020-09-18 14:53:38 +0000

    Data types

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,8 +2,31 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. Star Wars ");
-    System.out.println("2. spider man");
-    System.out.println("3. saving private Ryan");
-System.out.println("4. The Avengers");
-System.out.println("5. Jurassic park");
+       boolean likesBroccoli = false;
+       byte manyFingers = 5;
+       char favariteletter = 'r';
+       short whatIs = 10000;
+       int manyStudents = 2000000;
+       long popEarth = 780000000;
+       float mnPop = 5640000;
+       double whatS = 1000000000;
+
+System.out.println("what is 10^16?");
+System.out.println(whatS);
+System.out.println("what is minnesota population?");
+System.out.println(mnPop);
+System.out.println("what is the population of the earth?");
+System.out.println(popEarth);
+System.out.println("How many students are there in the world?");
+System.out.println(manyStudents);
+System.out.println("what is 100 X 100?");
+System.out.println(whatIs);
+System.out.println("what is my favorite letter?");
+System.out.println(favariteletter);
+System.out.println("how many fingers do humans have?");
+System.out.println(manyFingers);
+       System.out.println("Do I like broccoli?");
+       System.out.println(likesBroccoli);
+    }
+
+}

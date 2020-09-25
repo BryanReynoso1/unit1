0c3538dbaca682ad0be186b5821de7a044fe20d7
BryanReynoso1 <reynosobryan90@gmail.com>
2020-09-25 15:26:30 +0000

    Mad lib 2

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -7,20 +7,18 @@ public class App {
      
 Scanner input = new Scanner(System.in);
 
-System.out.println("Please enter your first name.");
-String firstName = input.nextLine();
-System.out.println("Please enter your last name.");
-String lastName = input.nextLine();
-System.out.println("Please enter your age.");
-String youAge = input.nextLine();
-System.out.println("Please enter your grade.");
-String youGrade = input.nextLine();
-System.out.println("Please enter your favorite color.");
-String youColor = input.nextLine();
-System.out.println("hello, " + firstName + " " + lastName);
-System.out.println("you are " + youAge);
-System.out.println("you are in " + youGrade);
-System.out.println("and your favorite color is " + youColor);
+System.out.println("Please enter a name");
+String name1 = input.nextLine();
+System.out.println("Please enter a adjective");
+String adjective1 = input.nextLine();
+System.out.println("Please enter a location");
+String location1 = input.nextLine();
+System.out.println("Please enter a verb");
+String verb1 = input.nextLine();
+System.out.println("Please enter a item");
+String item1 = input.nextLine();
+System.out.println(name1 + " is " + adjective1 + " because he is going to the " + location1);
+System.out.println("He is " + verb1 + " soccer with his friends so he brings his " + item1);
     }
 
 }

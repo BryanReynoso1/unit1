57c3c909b0b7fa7ffbef11aa0614819e71c0a4ea
BryanReynoso1 <reynosobryan90@gmail.com>
2020-10-05 15:03:45 +0000

    If statements

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,26 +3,17 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-     for (int i = 10; i <= 20; i = i + 1){
-         System.out.println("The count is " + i);
-         System.out.println("Let's keep counting.");
-     }
-     for (int l = 10; l <= 100; l = l + 10){
-         System.out.println("The count is " + l);
-         System.out.println("Lets keep counting");
-     }
-     for (int m = 1000; m <= 20056; m = m + 1000){
-         System.out.println("The count is " + m);
-         System.out.println("Lets keep counting.");
-     }
-     for (int t = 11; t <= 12; t = t + 1){
-         System.out.println("The count is " + t);
-         System.out.println("Lets keep counting.");
-     }
-     for (int o = 10; o >= 0; o = o - 1){
-         System.out.println("The count is " + o);
-         System.out.println("Lets keep counting.");
-     }
+        for (int i = 1; i <=20; i++){
+            if (i % 4 ==0){
+                System.out.println(i + " is divided by 4");
+            } else {
+                System.out.println(i + " is not divided by 4");
+            }
+        }
+        System.out.println("All finished");
+
+    
+     
 
      
 

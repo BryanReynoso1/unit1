d86d336e9962b3a986ef82e3e000821b800cac0f
BryanReynoso1 <reynosobryan90@gmail.com>
2020-10-12 19:25:48 +0000

    Challenges 1 & 2

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,45 +1,39 @@
 package app;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-System.out.println("Welcome to monkey facts, please choose a fact (1-5)");
-String fact1 = input.nextLine();
-String choosefact1 = "1";
-if (fact1.equals(choosefact1)){
-    System.out.println("There are over 2 different species of monkeys world which are old world monkeys and new wrold monkeys");
-} else {
-    System.out.println("Error");
-}
-String fact2 = input.nextLine();
-String choosefact2 = "2";
-if (fact2.equals(choosefact2)){
-    System.out.println("Monkeys live both on the ground and in trees");
-} else {
-    System.out.println("Error");
-}
-String fact3 = input.nextLine();
-String choosefact3 = "3";
-if (fact3.equals(choosefact3)){
-    System.out.println("A group of mokey is reffered to as a mission, tribe, or troop");
-} else {
-    System.out.println("Error");
-}
-String fact4 = input.nextLine();
-String choosefact4 = "4";
-if (fact4.equals(choosefact4)){
-    System.out.println("All species of monkeys have opposable thumbs just like humans");
-} else {
-    System.out.println("Error");
-}
-String fact5 = input.nextLine();
-String choosefact5 = "5";
-if (fact5.equals(choosefact5)){
-   System.out.println("Monkeys are mostly omnivores");
-} else {
-    System.out.println("Error");
-}
+   for (int i = 0; i <= 1000; i = i + 100){
+       System.out.println("the number is " + i);
+   }
+   System.out.println("Challenge 1 complete");
+   Scanner input = new Scanner(System.in);
+   System.out.println("Please enter a password");
+   String password1 = input.nextLine();
+   String choosepassword1 = "Stma2020";
+   if (password1.equals(choosepassword1)){
+       System.out.println("have a good day");
+   } else {
+       System.out.println("Error");
+   }
+   System.out.println("Please enter a password again");
+   String password2 = input.nextLine();
+   String choosepassword2 = "Stma2020";
+   if (password2.equals(choosepassword2)){
+       System.out.println("have a good day");
+   } else {
+       System.out.println("Error");
+   }
+   System.out.println("last try to get the secreat message, enter a password");
+   String password3 = input.nextLine();
+   String choosepasswrod3 = "Stma2020";
+   if (password3.equals(choosepasswrod3)){
+       System.out.println("have a good day");
+   } else {
+       System.out.println("Error, you dont get the secreat message");
+   }
+System.out.println("Challenge 2 complete");
 
 
 

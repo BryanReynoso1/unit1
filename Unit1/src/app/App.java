3488fafcdce355b033dfe3d254c165f8552b5ada
BryanReynoso1 <reynosobryan90@gmail.com>
2020-10-20 17:32:18 +0000

    Challenge 3 completed

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,37 +3,34 @@ package app;
 import java.util.Scanner;
 
 public class App {
+   
     public static void main(String[] args) throws Exception {
-   for (int i = 0; i <= 1000; i = i + 100){
-       System.out.println("the number is " + i);
-   }
-   System.out.println("Challenge 1 complete");
+   int targetNum = 55;
+   boolean isFinished = false;
+
    Scanner input = new Scanner(System.in);
-   System.out.println("Please enter a password");
-   String password1 = input.nextLine();
-   String choosepassword1 = "Stma2020";
-   if (password1.equals(choosepassword1)){
-       System.out.println("have a good day");
-   } else {
-       System.out.println("Error");
+
+   while(isFinished == false){
+   System.out.println("Guess a number 0 and 100");
+   int guess = input.nextInt();
+   input.nextLine();
+
+   if (guess == targetNum){
+       System.out.println("Correct");
+       isFinished = true;
    }
-   System.out.println("Please enter a password again");
-   String password2 = input.nextLine();
-   String choosepassword2 = "Stma2020";
-   if (password2.equals(choosepassword2)){
-       System.out.println("have a good day");
-   } else {
-       System.out.println("Error");
-   }
-   System.out.println("last try to get the secreat message, enter a password");
-   String password3 = input.nextLine();
-   String choosepasswrod3 = "Stma2020";
-   if (password3.equals(choosepasswrod3)){
-       System.out.println("have a good day");
-   } else {
-       System.out.println("Error, you dont get the secreat message");
-   }
-System.out.println("Challenge 2 complete");
+
+    else{
+
+        if(guess > targetNum){
+            System.out.println("High");
+        }
+        else{
+            System.out.println("Low");
+        }
+    }
+}
+
 
 
 
